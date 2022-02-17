cadd877fda2a3ea409db5a00a41ce0ec226271ec
utvecklare001 <99913092+utvecklare001@users.noreply.github.com>
2022-02-17 20:25:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forms0
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // skapar en lista av strängar
+            var länder = new List<string>();
+
+            länder.Add("Norge");
+            länder.Add("Danmark");
+            länder.Add("Island");
+            länder.Add("Finland");
+            länder.Add("Grönland");
+
+            // loopa igenom listelementen med en foreach-loop  
+            foreach (var land in länder)
+            {
+                Console.WriteLine(land);
+            }
+        }
+    }
+}
